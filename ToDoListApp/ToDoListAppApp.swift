cb465ef663b846e055bdd96b4506c5be05aab166
Shrushti-shetty <shrushti.shetty@robosoftin.com>
2023-11-27 14:58:24 +0530

    Inital Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Shrushti S Shetty on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
